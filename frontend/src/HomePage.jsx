eaf7ba7ac4cf8693cb1ae85cba16cb2f905618e7
pelasg0 <mrdrawin983@gmail.com>
2025-05-28 08:59:16 +0200

    height change

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -5,7 +5,7 @@ import chatImg from "./assets/only-logo-white.png";
 export default function HomePage({ onRegisterClick, onLoginClick }) {
   return (
     <main className="homepage-container">
-      <img src={chatImg} alt="Chat App" style={{ width: "400px", marginBottom: "1em" }} />
+      <img src={chatImg} alt="Chat App" style={{ width: "150px", marginBottom: "1em" }} />
       <h1>Welcome to Babel's Room!</h1>
       <p>The chat application where you can remain fully anonymous.</p>
       <div className="button-group">
